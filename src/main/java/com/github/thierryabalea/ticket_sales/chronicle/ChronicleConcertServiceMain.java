6900bc4a5ad44f63578c90db95972a4bcc09aded
Thierry ABALEA <t.abalea@gmail.com>
2016-04-16 15:56:38 +0200

    refacto: remove the ControllerThread classes to simplify the code

--- src/main/java/com/github/thierryabalea/ticket_sales/chronicle/ChronicleConcertServiceMain.java
+++ src/main/java/com/github/thierryabalea/ticket_sales/chronicle/ChronicleConcertServiceMain.java
@@ -3,19 +3,28 @@ package com.github.thierryabalea.ticket_sales.chronicle;
 import com.github.thierryabalea.ticket_sales.api.service.CommandHandler;
 import com.github.thierryabalea.ticket_sales.api.service.EventHandler;
 import com.github.thierryabalea.ticket_sales.domain.ConcertService;
+import net.openhft.affinity.AffinityLock;
 import net.openhft.chronicle.core.OS;
 import net.openhft.chronicle.queue.ChronicleQueue;
 import net.openhft.chronicle.queue.MethodReader;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
-
-import java.util.concurrent.ExecutorService;
+import net.openhft.chronicle.threads.LongPauser;
+import net.openhft.chronicle.threads.Pauser;
 
 import static java.lang.String.format;
-import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
 
-public class ChronicleConcertServiceMain {
+public class ChronicleConcertServiceMain extends Thread {
+
+    private final Pauser pauser = new LongPauser(1, 100, 500, 10_000, MICROSECONDS);
+    private MethodReader commandHandlerReader;
+    private MethodReader createConcertReader;
 
     public static void main(String[] args) throws Exception {
+        new ChronicleConcertServiceMain().main();
+    }
+
+    public void main() {
         String commandHandlerQueue = format("%s/%s", OS.TARGET, "commandHandlerQueue");
         String eventHandlerQueue = format("%s/%s", OS.TARGET, "eventHandlerQueue");
         String createConcertQueue = format("%s/%s", OS.TARGET, "createConcertQueue");
@@ -30,18 +39,35 @@ public class ChronicleConcertServiceMain {
             commandHandler = new ConcertService(eventHandler);
         }
 
-        ExecutorService executorService = newSingleThreadExecutor();
-
-        MethodReader commandHandlerReader;
         try (ChronicleQueue queue = SingleChronicleQueueBuilder.binary(commandHandlerQueue).build()) {
             commandHandlerReader = queue.createTailer().afterLastWritten(queue).methodReader(commandHandler);
         }
 
-        MethodReader createConcertReader;
         try (ChronicleQueue queue = SingleChronicleQueueBuilder.binary(createConcertQueue).build()) {
             createConcertReader = queue.createTailer().afterLastWritten(queue).methodReader(commandHandler);
         }
 
-        executorService.execute(new ConcertServiceControllerThread(commandHandlerReader, createConcertReader));
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        AffinityLock lock = AffinityLock.acquireLock();
+        try {
+            while (true) {
+                boolean didSomeWork = false;
+
+                didSomeWork |= commandHandlerReader.readOne();
+                didSomeWork |= createConcertReader.readOne();
+
+                if (didSomeWork) {
+                    pauser.reset();
+                } else {
+                    pauser.pause();
+                }
+            }
+        } finally {
+            lock.release();
+        }
     }
 }
--- src/main/java/com/github/thierryabalea/ticket_sales/chronicle/ChronicleWebMain.java
+++ src/main/java/com/github/thierryabalea/ticket_sales/chronicle/ChronicleWebMain.java
@@ -6,10 +6,13 @@ import com.github.thierryabalea.ticket_sales.api.service.CommandHandler;
 import com.github.thierryabalea.ticket_sales.web.RequestWebServer;
 import com.github.thierryabalea.ticket_sales.web.ResponseWebServer;
 import com.github.thierryabalea.ticket_sales.web.json.TicketPurchaseFromJson;
+import net.openhft.affinity.AffinityLock;
 import net.openhft.chronicle.core.OS;
 import net.openhft.chronicle.queue.ChronicleQueue;
 import net.openhft.chronicle.queue.MethodReader;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.threads.LongPauser;
+import net.openhft.chronicle.threads.Pauser;
 import org.jctools.queues.MpscArrayQueue;
 import org.rapidoid.http.fast.On;
 
@@ -17,42 +20,76 @@ import java.util.concurrent.ExecutorService;
 
 import static java.lang.String.format;
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
 
-public class ChronicleWebMain {
+public class ChronicleWebMain extends Thread {
+
+    private final Pauser pauser = new LongPauser(1, 100, 500, 10_000, MICROSECONDS);
+    private MpscArrayQueue<Poll> pollQueue;
+    private ResponseWebServer responseWebServer;
+    private MethodReader eventHandlerReader;
 
     public static void main(String[] args) throws Exception {
+        new ChronicleWebMain().main();
+    }
+
+    public void main() throws Exception {
         On.port(7070);
 
-        MpscArrayQueue<Poll> pollQueue = new MpscArrayQueue<>(1024);
-        ResponseWebServer responseWebServer = new ResponseWebServer();
+        pollQueue = new MpscArrayQueue<>(1024);
+        responseWebServer = new ResponseWebServer();
         ResponseWebServer.PollHandler pollHandler = (accountId, version) -> pollQueue.offer(new Poll(accountId, version));
         responseWebServer.init(pollHandler);
 
         String commandHandlerQueue = format("%s/%s", OS.TARGET, "commandHandlerQueue");
         String eventHandlerQueue = format("%s/%s", OS.TARGET, "eventHandlerQueue");
 
-        ExecutorService executorService = newSingleThreadExecutor();
         try (ChronicleQueue queue = SingleChronicleQueueBuilder.binary(commandHandlerQueue).build()) {
             CommandHandler commandHandler = queue.createAppender()
                     .methodWriterBuilder(CommandHandler.class)
                     .recordHistory(true)
                     .get();
 
-            RequestWebServer.JsonRequestHandler requestHandler = request -> {
-                executorService.execute(() -> {
-                    TicketPurchase ticketPurchase = TicketPurchaseFromJson.fromJson(request);
-                    commandHandler.onTicketPurchase(ticketPurchase);
-                });
-            };
+            ExecutorService executorService = newSingleThreadExecutor();
+            RequestWebServer.JsonRequestHandler requestHandler = request -> executorService.execute(() -> {
+                TicketPurchase ticketPurchase = TicketPurchaseFromJson.fromJson(request);
+                commandHandler.onTicketPurchase(ticketPurchase);
+            });
 
             RequestWebServer requestWebServer = new RequestWebServer(requestHandler);
             requestWebServer.init();
         }
 
         try (ChronicleQueue queue = SingleChronicleQueueBuilder.binary(eventHandlerQueue).build()) {
-            MethodReader reader = queue.createTailer().afterLastWritten(queue).methodReader(responseWebServer);
-            Thread controller = new WebControllerThread(reader, pollQueue, responseWebServer::onPoll);
-            controller.run();
+            eventHandlerReader = queue.createTailer().afterLastWritten(queue).methodReader(responseWebServer);
+        }
+
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        AffinityLock lock = AffinityLock.acquireLock();
+        try {
+            while (true) {
+                boolean didSomeWork;
+
+                didSomeWork = eventHandlerReader.readOne();
+
+                Poll poll = pollQueue.poll();
+                if (poll != null) {
+                    responseWebServer.onPoll(poll.accountId, poll.version);
+                    didSomeWork = true;
+                }
+
+                if (didSomeWork) {
+                    pauser.reset();
+                } else {
+                    pauser.pause();
+                }
+            }
+        } finally {
+            lock.release();
         }
     }
 }
